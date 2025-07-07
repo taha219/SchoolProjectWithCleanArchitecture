0c866f2f3c1785f4fd448baae65bab6be27b8ab0
taha <to44327@gmail.com>
2025-07-07 17:25:14 +0300

    feat<email>:send email command

---
+++ SchoolProject.Api/Controllers/EmailController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Api.Base;
+using SchoolProject.Core.Feature.Email.Commands.Models;
+
+namespace SchoolProject.Api.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = "Admin,User")]
+    public class EmailsController : AppControllerBase
+    {
+        [HttpPost("SendEmail")]
+        public async Task<IActionResult> SendEmail([FromQuery] SendEmailCommand command)
+        {
+            var response = await Mediator.Send(command);
+            return NewResult(response);
+        }
+    }
+}
---
+++ SchoolProject.Core/Feature/Email/Commands/Models/SendEmailCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SchoolProject.Core.Bases;
+
+namespace SchoolProject.Core.Feature.Email.Commands.Models
+{
+    public class SendEmailCommand : IRequest<ApiResponse<string>>
+    {
+        public string Email { get; set; }
+        public string Messege { get; set; }
+    }
+}
